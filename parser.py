67e2c650d2efa32e1b5676153f49ff4587ab77e3
the-alex-g <alex.gestwicki@gmail.com>
2024-03-15 21:05:54 -0400

    Complete basic monster parsing
    can only handle abilities and actions so far.

--- parser.py
+++ parser.py
@@ -95,6 +95,13 @@ CR_TO_XP = {
     30:"155,000",
 }
 NEWLINE = "\\\\"
+LINEBREAK = NEWLINE + "\\bigskip"
+PREAMBLE = """\\documentclass[letterpaper, 12pt, twocolumn]{book}
+\\usepackage{ragged2e}
+\\usepackage[left=0.5in, right=0.5in, top=1in, bottom=1in]{geometry}
+\\usepackage{graphicx}
+\\begin{document}\\RaggedRight"""
+CONCLUSION = """\\end{document}"""
 
 
 filedict = {}
@@ -121,7 +128,7 @@ def diceroll(num, size, bonus):
 
 def save(ability, abilitybonus, profbonus):
     dc = 8 + abilitybonus + profbonus
-    return "DC " + str(dc) + " " + ABILITIES_SPELLOUT[ability] + " saving throw"
+    return "DC " + str(dc) + " " + ABILITIES_SPELLOUT[ability[2:]] + " saving throw"
 
 
 def hitpoints(num, size, conbonus):
@@ -140,6 +147,13 @@ def cr(cr):
 
 def score_to_bonus(score):
     return floor(score / 2) - 5
+
+
+def format_bonus(bonus):
+    if bonus >= 0:
+        return "+" + str(bonus)
+    else:
+        return str(bonus)
 
 
 def ability_scores_to_bonuses(stats):
@@ -171,10 +185,8 @@ def skill_profs(skill_prof_list, stats, profbonus):
             skill_profs_string += ", "
         bonus = skill_profs_dict[skill]
         skill_profs_string += SKILL_PRETTYNAME[skill] + " "
-        if bonus >= 0:
-            skill_profs_string += "+"
-        skill_profs_string += str(bonus)
-    return skill_profs_string
+        skill_profs_string += format_bonus(bonus)
+    return {"string":skill_profs_string, "dict":skill_profs_dict}
 
 
 def save_profs(save_prof_list, stats, profbonus):
@@ -190,21 +202,23 @@ def save_profs(save_prof_list, stats, profbonus):
             save_profs_string += ", "
         bonus = save_profs_dict[save]
         save_profs_string += save.capitalize() + " "
-        if bonus >= 0:
-            save_profs_string += "+"
-        save_profs_string += str(bonus)
+        save_profs_string += format_bonus(bonus)
     return save_profs_string
 
 
-def format_and_execute(field, stats):
+def format_and_execute(field, stats, profbonus):
     formatted_field = ""
     in_function_body = False
     arg_text = ""
+    function_name = ""
     for char in field:
         if char == " ":
             if in_function_body:
                 if arg_text in ABILITY_LIST:
                     arg_text = str(stats[arg_text])
+                elif not arg_text.isdigit():
+                    formatted_field += "\""
+                    arg_text += "\""
                 formatted_field += arg_text + ", "
                 arg_text = ""
             else:
@@ -215,12 +229,16 @@ def format_and_execute(field, stats):
                 arg_text += char
             else:
                 formatted_field += char
+                function_name += char
     if arg_text in ABILITY_LIST:
         arg_text = str(stats[arg_text])
-    return eval(formatted_field + arg_text + ")")
+    formatted_field += arg_text
+    if function_name == "save":
+        formatted_field += ", " + str(profbonus)
+    return eval(formatted_field + ")")
 
 
-def resolve_functions(string, stats):
+def resolve_functions(string, stats, profbonus):
     updated_string = ""
     field = ""
     field_started = False
@@ -229,7 +247,7 @@ def resolve_functions(string, stats):
             field_started = True
         elif char == "]":
             field_started = False
-            updated_string += format_and_execute(field, stats)
+            updated_string += format_and_execute(field, stats, profbonus)
             field = ""
         elif field_started:
             field += char
@@ -239,35 +257,47 @@ def resolve_functions(string, stats):
 
 
 def create_stat_table(scores, bonuses):
-    table = "\\begin{footnotesize}\\noindent\\begin{tabular}{llllll}\\hline" + NEWLINE
+    table = """\\smallskip
+    \\begin{footnotesize}
+    \\noindent
+    \\resizebox{\\columnwidth}{!}{
+    \\begin{tabular}{llllll}
+    \\hline""" + NEWLINE
     for ability in ABILITY_LIST:
         table += "\\textbf{" + ability.upper() + "}"
         if ability != "cha":
             table += "&"
     table += NEWLINE
     for ability in ABILITY_LIST:
-        table += str(scores[ability]) + " ("
-        bonus = bonuses[ability]
-        if bonus >= 0:
-            table += "+"
-        table += str(bonus) + ")"
+        table += str(scores[ability]) + " (" + format_bonus(bonuses[ability]) + ")"
         if ability != "cha":
             table += "&"
-    table += NEWLINE + NEWLINE + "\\hline\\end{tabular}\\end{footnotesize}"
+    table += NEWLINE + NEWLINE + """\\hline
+    \\end{tabular}}
+    \\end{footnotesize}
+    \\smallskip"""
     return table
 
 
 def create_attack(attack, stats, profbonus):
     attack_string = "\\noindent\\textit{\\textbf{" + attack["name"] + ".} "
-    if attack["type"] == "mwa":
+    bonus = stats[attack["ability"]] + profbonus
+    if attack["type"] == "mw":
         attack_string += "Melee Weapon Attack:} "
-        bonus = stats[attack["ability"]] + profbonus
-        if bonus >= 0:
-            attack_string += "+"
-        attack_string += str(bonus) + " to hit, reach " + str(attack["reach"]) + "ft., " + attack["target"] + "." + NEWLINE
-    attack_string += "\\textit{Hit:} " + resolve_functions(attack["onhit"], stats)
+        attack_string += format_bonus(bonus) + " to hit, reach " + str(attack["reach"]) + "ft."
+    elif attack["type"] == "rw":
+        attack_string += "Ranged Weapon Attack:} "
+        attack_string += format_bonus(bonus) + " to hit, range " + str(attack["reach"])
+    elif attack["type"] == "ms":
+        attack_string += "Melee Spell Attack:} "
+        attack_string += format_bonus(bonus) + " to hit, reach " + str(attack["reach"]) + "ft."
+    elif attack["type"] == "rw":
+        attack_string += "Ranged Spell Attack:} "
+        attack_string += format_bonus(bonus) + " to hit, range " + str(attack["reach"])
+        
+    attack_string += ", " + attack["target"] + "." + NEWLINE + "\\textit{Hit:} " + resolve_functions(attack["onhit"], stats, profbonus)
     if "special" in attack:
-        attack_string += NEWLINE + attack["special"]
+        attack_string += NEWLINE + resolve_functions(attack["special"], stats, profbonus)
     return attack_string
 
 
@@ -286,6 +316,18 @@ def dmg_attributes(attributes):
     return attribute_string
 
 
+def speeds(speed_dict):
+    speed_string = str(speed_dict["land"]) + " ft."
+    speed_type_list = sorted(speed_dict)
+    for speed_type in speed_type_list:
+        if speed_type != "land":
+            if speed_type == "fly-hover":
+                speed_string += ", fly " + str(speed_dict[speed_type]) + " ft. (hover)"
+            else:
+                speed_string += ", " + speed_type + " " + str(speed_dict[speed_type]) + " ft."
+    return speed_string
+    
+
 def create_monster(monster):
     monster_string = "\\subsection*{"
     if "headername" in monster:
@@ -293,8 +335,15 @@ def create_monster(monster):
     else:
         monster_string += monster["name"]
     monster_string += "}"
+
     if "flavor" in monster:
-        monster_string += "\\textit{" + monster["flavor"] + "}" + NEWLINE + NEWLINE
+        monster_string += "\\textit{" + monster["flavor"] + "}" + LINEBREAK
+
+    if "description" in monster:
+        for description in monster["description"]:
+            monster_string += "\\textbf{\\textit{" + description["header"] + ".}} "
+            monster_string += description["text"] + NEWLINE
+        monster_string += "\\bigskip"
 
     monster_string += "\\textbf{" + monster["name"].upper() + "}" + NEWLINE
     monster_string += "\\textit{" + (monster["size"] + " " + monster["type"] + ", " + monster["alignment"]).title() + "}" + NEWLINE
@@ -312,21 +361,19 @@ def create_monster(monster):
 
     monster_string += "\\textbf{Hit Points} " + hitpoints(monster["hd"], monster["size"], bonuses["con"]) + NEWLINE
 
-    monster_string += "\\textbf{Speed} "
-    for speed_type in monster["speed"]:
-        if speed_type == "land":
-            monster_string += str(monster["speed"][speed_type]) + " ft."
-        else:
-            monster_string += ", " + speed_type + " " + str(monster["speed"][speed_type]) + " ft."
-    monster_string += NEWLINE
+    monster_string += "\\textbf{Speed} " + speeds(monster["speed"]) + NEWLINE
+    
 
     monster_string += create_stat_table(scores, bonuses) + NEWLINE
 
     if "saves" in monster:
-        monster_string += "\\textbf{Saving Throws} " + save_profs(monster["saves", bonuses, profbonus]) + NEWLINE
+        monster_string += "\\textbf{Saving Throws} " + save_profs(monster["saves"], bonuses, profbonus) + NEWLINE
 
+    skill_prof_dict = {}
     if "skills" in monster:
-        monster_string += "\\textbf{Skills} " + skill_profs(monster["skills"].sort(), bonuses, profbonus) + NEWLINE
+        skills = skill_profs(sorted(monster["skills"]), bonuses, profbonus)
+        skill_prof_dict = skills["dict"]
+        monster_string += "\\textbf{Skills} " + skills["string"] + NEWLINE
 
     if "vulnerable" in monster:
         monster_string += "\\textbf{Damage Vulnerabilities} " + dmg_attributes(monster["vulnerable"]) + NEWLINE
@@ -338,41 +385,111 @@ def create_monster(monster):
         monster_string += "\\textbf{Damage Immunities} " + dmg_attributes(monster["immune"]) + NEWLINE
 
     if "cond-immune" in monster:
-        monster_string += "\\textbf{Condition Immunities} " + comma_separate(monster["cond-immune"]) + NEWLINE
+        monster_string += "\\textbf{Condition Immunities} " + comma_separate(sorted(monster["cond-immune"])) + NEWLINE
     
     monster_string += "\\textbf{Senses} "
     if "senses" in monster:
-        monster_string += monster["senses"] + ", "
-    monster_string += "passive Perception " + str(10) + NEWLINE # this is a problem
+        monster_string += comma_separate(sorted(monster["senses"])) + ", "
+    monster_string += "passive Perception "
+    if "perception" in skill_prof_dict:
+        monster_string += str(10 + skill_prof_dict["perception"])
+    else:
+        monster_string += str(10 + bonuses["wis"])
+    monster_string += NEWLINE
     
     monster_string += "\\textbf{Languages "
     if "languages" in monster:
-        monster_string += "}" + monster["languages"]
+        monster_string += "}" + comma_separate(sorted(monster["languages"])).title()
     else:
         monster_string += "---}"
     monster_string += NEWLINE
 
-    monster_string += "\\textbf{Challenge} " + cr(monster["cr"])
+    monster_string += "\\textbf{Challenge} " + cr(monster["cr"]) + LINEBREAK
+
+    if "abilities" in monster:
+        ability_name_dict = {}
+        for ability in monster["abilities"]:
+            ability_name_dict[ability["name"]] = ability
+        for ability_name in sorted(ability_name_dict):
+            ability = ability_name_dict[ability_name]
+            monster_string += "\\textbf{\\textit{" + ability["name"] + ".}} "
+            monster_string += resolve_functions(ability["effect"], bonuses, profbonus) + LINEBREAK
+
+    if "attacks" in monster or "actions" in monster:
+        monster_string += "\\textbf{Actions}" + NEWLINE
+
+    monster_string += "\n%mabp\n"
+
+    if "attacks" in monster:
+        attack_name_dict = {}
+        for attack in monster["attacks"]:
+            attack_name_dict[attack["name"]] = attack
+        for attack_name in sorted(attack_name_dict):
+            monster_string += create_attack(attack_name_dict[attack_name], bonuses, profbonus) + LINEBREAK
+
+    if "actions" in monster:
+        action_name_dict = {}
+        for action in monster["actions"]:
+            if action["name"].lower() != "multiattack":
+                action_name_dict[action["name"]] = action
+            else:
+                monster_string = monster_string.replace(
+                    "%mabp",
+                    "\\textbf{\\textit{Multiattack.}} " + resolve_functions(action["effect"], bonuses, profbonus) + LINEBREAK
+                )
+        for action_name in sorted(action_name_dict):
+            action = action_name_dict[action_name]
+            monster_string += "\\textbf{\\textit{" + action["name"] + ".}} "
+            monster_string += resolve_functions(action["effect"], bonuses, profbonus) + LINEBREAK
+            
 
     print(monster_string)
 
 
+def create_doc(filedict):
+    print(PREAMBLE)
     
-            
+    monster_name_dict = {}
+    monsters_by_habitat = {}
+    monsters_by_type = {}
+    monsters_by_cr = {}
+    for monster in filedict["monsters"]:
+        monstername = ""
+        if "headername" in monster:
+            monstername = monster["headername"]
+        else:
+            monstername = monster["name"]
+        if "habitat" in monster:
+            for region in monster["habitat"]:
+                if region in monsters_by_habitat:
+                    monsters_by_habitat[region].append(monstername)
+                else:
+                    monsters_by_habitat[region] = [monstername]
+        elif "any" in monsters_by_habitat:
+            monsters_by_habitat["any"].append(monstername)
+        else:
+            monsters_by_habitat["any"] = [monstername]
+
+        if monster["type"] in monsters_by_type:
+            monsters_by_type[monster["type"]].append(monstername)
+        else:
+            monsters_by_type[monster["type"]] = [monstername]
+        if monster["cr"] in monsters_by_cr:
+            monsters_by_cr[monster["cr"]].append(monstername)
+        else:
+            monsters_by_cr[monster["cr"]] = [monstername]
+        monster_name_dict[monstername] = monster
+
+    for monster_name in sorted(monster_name_dict):
+        monster = monster_name_dict[monster_name]
+        create_monster(monster)
+
+    print(CONCLUSION)
+
 
 with open("monsters.yaml") as stream:
     try:
-        filedict = yaml.safe_load(stream)
+        create_doc(yaml.safe_load(stream))
     except yaml.YAMLError as exc:
         print(exc)
-
-#print(diceroll(2, 4, 1))
-#print(hitpoints (10, "small", 1))
-#print(save("str", 2, 2))
-#print(format_and_execute("diceroll 2 4 str", {"str":2}))
-#print(skill_profs(["athletics", "acrobatics", "acrobatics"], {"str":-3, "dex":1}, 2))
-#print(save_profs(["str", "dex", "dex"], {"str":-3, "dex":1}, 2))
-#print(score_to_bonus(10))
-
-create_monster(filedict)
 
