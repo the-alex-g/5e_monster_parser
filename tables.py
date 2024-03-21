2995b2a769313cf00628d179a915b8a365f4a4c1
Alex G <alex.gestwicki@gmail.com>
2024-03-21 17:12:41 -0400

    Add some more monsters

--- tables.py
+++ tables.py
@@ -242,5 +242,7 @@ PREBAKED_ABILITIES = {
     "Siege Monster":"The [name] deals double damage to structures.",
     "Swarm":"The [name] can fit through any gap large enough for a [swarmsize] creature, cannot regain hit points or gain temporary hit points, takes twice as much damage from area-of-effect sources, can enter the spaces of other creatures, and other creatures can enter the swarm's space.",
     "Rooted":"The [name] has advantage on saving throws made to resist being moved or knocked prone.",
-    "Stench":"Any creature other than a [name] that starts its turn within 5 feet of the [name] must succeed on a [save w/con con], being poisoned until the start of the creature’s next turn. On a successful saving throw, the creature is immune to the stench of all [name]s for 1 hour."
+    "Stench":"Any creature other than a [name] that starts its turn within 5 feet of the [name] must succeed on a [save w/con con], being poisoned until the start of the creature’s next turn. On a successful saving throw, the creature is immune to the stench of all [name]s for 1 hour.",
+    "Amphibious":"The [name] can breathe both air and water with equal ease.",
+    "Aquatic":"The [name] can only breathe underwater.",
 }
